802b21e903c37c92f356c89a03f95b81f164d04e
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-04-12 22:49:32 +0800

    chore: remove unused frames package

--- frames/app/only-frames-view/frames/frames.ts
+++
@@ -1,5 +0,0 @@
-import { createFrames, types } from "frames.js/next";
-
-export const frames = createFrames({
-  basePath: "/only-frames-view/frames",
-});
--- frames/app/page.tsx
+++ frames/app/page.tsx
@@ -1,50 +1,119 @@
-import {
-  NextServerPageProps
-} from "frames.js/next/server";
-import Link from "next/link";
-import { currentURL } from "./utils";
+import { getFrameMetadata } from 'frog/next'
+import type { Metadata } from 'next'
+import Image from 'next/image'
 
-type State = {
-  active: string;
-  total_button_presses: number;
-};
-
-import { fetchMetadata } from "frames.js/next";
-import type { Metadata } from "next";
-import { createDebugUrl } from "./debug";
-import { vercelURL } from "./utils";
+import styles from './page.module.css'
 
 export async function generateMetadata(): Promise<Metadata> {
+  const frameTags = await getFrameMetadata(
+    `${process.env.VERCEL_URL || 'http://localhost:3000'}/api`,
+  )
   return {
-    title: "frames.js starter",
-    description: "This is a frames.js starter template",
-    other: {
-      ...(await fetchMetadata(
-        new URL(
-          "/frames",
-          vercelURL() || "http://localhost:3000"
-        )
-      )),
-    },
-  };
+    other: frameTags,
+  }
 }
 
-// This is a react server component only
-export default async function Home({ searchParams }: NextServerPageProps) {
-  const url = currentURL("/");
-
-  // then, when done, return next frame
+export default function Home() {
   return (
-    <div className="p-4">
-      frames.js starter kit. The Template Frame is on this page, it&apos;s in
-      the html meta tags (inspect source).{" "}
-      <Link href={createDebugUrl(url)} className="underline">
-        Debug
-      </Link>{" "}
-      or see{" "}
-      <Link href="/examples" className="underline">
-        other examples
-      </Link>
-    </div>
-  );
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 8 }}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>app/page.tsx</code>
+          </p>
+          <p>
+            Head to{' '}
+            <a
+              href="/api/dev"
+              style={{ display: 'inline', fontWeight: 'semibold' }}
+            >
+              <code className={styles.code}>localhost:3000/api</code>
+            </a>{' '}
+            for your frame endpoint.
+          </p>
+        </div>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
 }
