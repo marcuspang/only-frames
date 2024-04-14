3928567755378004f9c2b8ff546110d410136cab
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-04-14 13:16:19 +0800

    chore: remove unused ts-expect-error directives

--- frames/lib/prisma.ts
+++ frames/lib/prisma.ts
@@ -13,12 +13,9 @@ declare global {
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  // @ts-expect-error
   if (!global.prisma) {
-    // @ts-expect-error
     global.prisma = new PrismaClient();
   }
-  // @ts-expect-error
   prisma = global.prisma;
 }
 
